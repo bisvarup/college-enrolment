74540f1225dfdb440660fa403e626d743a325dc6
bisvarup <bisvarup.mukherjee@tracxn.com>
2019-12-09 10:25:32 +0530

    login wip

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 from flask_dotenv import DotEnv
 import sys
 
 app = Flask(__name__)
+db = None
 try:
     print("Trying to connect to database")
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@db/db'
@@ -17,7 +19,16 @@ env = DotEnv()
 
 env.init_app(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
 '''
 This import is at the bottom to avoid circular dependency
 '''
-from app import routes
+from app import routes, User
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
-from app import app
+from flask import render_template, request, flash, redirect, url_for
+from app import app,db,User
 
 
 @app.route("/")
 @app.route("/index")
 def index():
     return "Hello, World!"
+
+@app.route('/register' , methods=['GET','POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    user = User(request.form['username'] , request.form['password'],request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    flash('User successfully registered')
+    return redirect(url_for('login'))
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    return redirect(url_for('index'))
