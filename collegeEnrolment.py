f5d7e8e996790a3c6a04a9362a8e29b8fd8e80a6
bisvarup <bisvarup.mukherjee@tracxn.com>
2019-12-11 14:24:42 +0530

    cleanup + bashscript to run project

--- collegeEnrolment.py
+++ collegeEnrolment.py
@@ -1,18 +1,15 @@
 import os
 from app import app, db
 
-basedir = os.path.join(os.path.abspath(os.path.dirname(__file__)),"../","db")
+basedir = "db"
 db_name = "app.db"
 db_file_path = os.path.join(basedir, db_name)
 
-print("Is db file present ",os.path.isfile(db_file_path))
+print("Checking if database is present ",os.path.isfile(db_file_path))
 '''
 Check if the db file is present, if is not present then
 create database
 '''
 if not os.path.isfile(db_file_path):
+    print("Creating database")
     db.create_all()
-
-app.debug = True
-app.run()
-
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+env FLASK_APP=collegeEnrolment.py FLASK_ENV=development flask run
